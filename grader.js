c52057bcddbfb2a03a48aecce7a47e7233427bd6
Ubuntu <ubuntu@ip-10-164-64-123.ec2.internal>
2013-07-09 20:14:46 +0000

    Worked on grader.js, currently broken

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,14 +45,26 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkUrl = function()
+{
+};
+
+var checkHtmlFile = function(htmlfile, checksfile, myUrl) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+    if( myUrl == URL_DEFAULT )
+    {
+        $ = cheerioHtmlFile(htmlfile);
+        for(var ii in checks) 
+        {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
     }
+    else
+    {
+        /*rest.get(myUrl).on('complete', */
+    }	
     return out;
 };
 
@@ -65,8 +78,10 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'url of file to check', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    
+    var checkJson = checkHtmlFile(program.file, program.checks, program.url);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
